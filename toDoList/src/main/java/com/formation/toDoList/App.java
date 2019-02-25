f01dd58d38e4f26821eae0a83ae033b0dd3f71d0
jefcx <blancjeremy@laposte.net>
2019-02-25 16:25:51 +0100

    Initialisation du projet

---
+++ toDoList/src/main/java/com/formation/toDoList/App.java
@@ -0,0 +1,18 @@
+package com.formation.toDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * VTC
+ * @author: Julie-Claire, Melody, Logithan, Jeremy (Aelion)
+ * @description: Back-end, gestion vtc
+ * @version: 1.0.0
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args ) {
+    	SpringApplication.run(App.class, args);
+    }
+}
